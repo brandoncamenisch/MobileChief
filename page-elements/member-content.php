481ea1188f835027bdd2ae53094275f70e8981a8
bc <mountaininja@gmail.com>
2012-08-22 18:54:14 -0600

    Added end section element instead of creating one for every element.

--- page-elements/member-content.php
+++ page-elements/member-content.php
@@ -1,9 +1,8 @@
 <?php
 
 /* ---------------------------------------------------------------------------- */
-/* Add Member Element to the Page Elements Menu
+/* Add STARTING Member Element to the Page Elements Menu
 /* ---------------------------------------------------------------------------- */
-
 	function plchf_add_member_content_element_member_content() {
 	
 		plchf_msb_add_page_element('Member Content');
@@ -15,7 +14,6 @@
 /* ---------------------------------------------------------------------------- */
 /* Create Settings for the Member Content
 /* ---------------------------------------------------------------------------- */
-
 	function plchf_msb_page_element_settings_member_content($count, $values){
 		
 		// Define Element Type
@@ -26,20 +24,20 @@
 			
 			'field' 	=> array(
 				'type' 			=> 'text',
-				'name' 			=> 'Enter some text',
-				'id' 			=> '_member_content_',
-				'tooltip' 		=> 'Enter your RSS feed URL',
-				'placeholder' 	=> 'Enter your RSS feed t to add it to the page',
+				'name' 			=> 'Enter a password for this content',
+				'id' 			=> '_member_password_',
+				'tooltip' 		=> 'Enter your password',
+				'placeholder' 	=> 'Enter your password',
 			),
 		);
 		$fields[] = array(
 			
 			'field' 	=> array(
 				'type' 			=> 'text',
-				'name' 			=> 'How many results',
-				'id' 			=> '_rss_results_',
-				'tooltip' 		=> 'How many results do you want to display?',
-				'placeholder' 	=> 'How many results do you want to display?',
+				'name' 			=> 'Content',
+				'id' 			=> '_member_content_',
+				'tooltip' 		=> 'What content would you like to display',
+				'placeholder' 	=> 'Which content do you want to display?',
 			),
 		
 		);
@@ -54,38 +52,38 @@
 /* ---------------------------------------------------------------------------- */
 /* Display Output for the Member Element
 /* ---------------------------------------------------------------------------- */
-
 	function plchf_msb_page_element_output_member_content($values) {
+		//Refer back to this URL upon form submission
+		 $pageURL = 'http';
+		 if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+		 $pageURL .= "://";
+		 if ($_SERVER["SERVER_PORT"] != "80") {
+		  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		 } else {
+		  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		 }
+		//Get the user defined static password
+		$password = $values['_member_password_'];
+		if (isset($_POST['textfield']) && $_POST['textfield'] == $values['_member_password_']) {
 		
- $pageURL = 'http';
- if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
- $pageURL .= "://";
- if ($_SERVER["SERVER_PORT"] != "80") {
-  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- } else {
-  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
- }
-
-$password = 'abc';
-if (isset($_POST['textfield']) && $_POST['textfield'] == 'abc') {
-    echo username();
-    echo $pageURL;
-}
-
-echo '
-<form name="form1" method="post" action="">
-  <p>
-    <label>
-      <input type="text" name="textfield" id="textfield">
-    </label>
-  </p>
-  <p>
-    <label>
-      <input type="submit" name="button" id="button" value="Submit">
-    </label>
-  </p>
-</form>';
-
+		echo  $values['_member_content_'];
+		} else {
+		
+		echo '
+		<form name="form1" method="post" action="">
+		  <p>
+		    <label>
+		      <input type="text" name="textfield" id="textfield">
+		    </label>
+		  </p>
+		  <p>
+		    <label>
+		      <input type="submit" name="button" id="button" value="Submit" class="btn">
+		    </label>
+		  </p>
+		</form>
+		<div style="display:none;>';
+		}
 		echo apply_filters('plchf_msb_page_element_output_member_content_filter',$output);
 		
 	}
