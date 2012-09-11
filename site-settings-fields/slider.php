34f0068971bc916c3d30f2da744b68f437e6bc6a
bc <mountaininja@gmail.com>
2012-09-11 17:31:41 -0600

    slider

---
+++ site-settings-fields/slider.php
@@ -0,0 +1,36 @@
+<?php
+
+/* ---------------------------------------------------------------------------- */
+/* Text Field
+/* ---------------------------------------------------------------------------- */
+
+function plchf_msb_site_settings_field_slider($fields, $count) {
+
+	// Get the Element Type
+	$element_type 	= $element_type;
+
+	// Get the Field Definitions
+	$type 				= $fields['type'];
+	$label 				= $fields['name'];
+	$tooltip	 		= $fields['tooltip'];
+	$placeholder	= $fields['placeholder'];
+	$field_id			= $fields['id'];
+
+	// Get the saved Value
+	$value			= $values[''.$field_id.''];
+
+	$output .= '
+	<label>'.$label.'
+		<a href="#" class="tipsy-se floatr" original-title="'.$tooltip.'">
+			<img src="'.PLUGINCHIEFMSB.'images/element-icons/element-info.png" width="20px">
+		</a>
+	</label>
+	';
+
+	$output .= '<div class="plchf_msb_slider_field"></div>';
+	$output .= '<input type="hidden" class="plchf_msb_slider_amount" name="field['.$element_type.'_'.$count.']['.$field_id.']" value="'.$value.'">';
+
+	echo apply_filters('plchf_msb_site_settings_field_slider_area_filter', $output);
+}
+
+add_action('plchf_msb_site_settings_field_slider','plchf_msb_site_settings_field_slider', 10, 4);
