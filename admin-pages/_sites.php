5656884c993ef1c940f57ac8d8185d8f0db86718
bc <mountaininja@gmail.com>
2012-09-11 16:03:44 -0600

    On plugin activation was giving undefined variable.

--- admin-pages/_sites.php
+++ admin-pages/_sites.php
@@ -5,21 +5,21 @@
 /* ---------------------------------------------------------------------------- */
 
 	function pluginchief_mobile_site_builder_menu() {
-		
+
 		add_menu_page(
-			'Mobile Site Builder', 
-			'Mobile Site Builder', 
-			'add_users', 
-			'pluginchiefmsb', 
-			'pluginchiefmsb_msb_my_sites_page_content',   
-			PLUGINCHIEFMSB . '/images/msb-icon.png', 
+			'Mobile Site Builder',
+			'Mobile Site Builder',
+			'add_users',
+			'pluginchiefmsb',
+			'pluginchiefmsb_msb_my_sites_page_content',
+			PLUGINCHIEFMSB . '/images/msb-icon.png',
 			90
 		);
-	
+
 	}
-	
+
 	add_action('admin_menu', 'pluginchief_mobile_site_builder_menu');
-	
+
 /* ---------------------------------------------------------------------------- */
 /* Create the Menu Page
 /* ---------------------------------------------------------------------------- */
@@ -28,193 +28,193 @@ function pluginchiefmsb_msb_my_sites_page_content() {
 
 ?>
 
-		<?php 
-		
-		get_pluginchiefmsb_header(); 
-			
+		<?php
+
+		get_pluginchiefmsb_header();
+
 		?>
-	
+
 		<div class="pluginchiefmsb-wrapper" id="pluginchiefmsb-wrapper">
-		
+
 			<div class="pluginchiefmsb-wrap">
-				
+
 				<?php do_action('plchf_msb_my_sites_above_page_title'); ?>
-					
+
 				<div class="clear"></div><!-- Clear -->
-					
+
 				<div class="settings-title">
-					
+
 					<h3 class="section-title floatl">Mobile Sites</h3>
-				
+
 					<a class="button-primary button btn btn-primary floatr" href="<?php echo apply_filters('plchf_msb_new_sites_page', 'admin.php?page=pluginchiefmsb/new-site' ); ?>"><?php echo apply_filters('plchf_msb_create_new_site_link_text', 'Create New Site'); ?></a>
-					
+
 				</div>
-				
+
 				<div class="clear"></div><!-- Clear -->
-				
+
 				<?php do_action('plchf_msb_my_sites_below_page_title'); ?>
-				
+
 				<hr>
-				
+
 				<div id="sites-list">
-					
+
 					<?php
-					
+
 					$pluginchiefmsb_args = array(
 						'post_type' 	=> 'pluginchiefmsb-sites',
 						'post_parent'	=> 0
 					);
-					
+
 					$sites = get_posts( $pluginchiefmsb_args );
-					
+
 					if ($sites) {
-					
+
 						foreach ( $sites as $site ) {
-							
+
 						$home_id 	= get_post_meta($site->ID, '_homepage_', true);
 						$site_theme = get_post_meta($site->ID, '_plchf_msb_site_theme', true);
 						$site_theme = ucwords(str_ireplace('_',' ', $site_theme));
-						
+
 						$siteid = $site->ID;
-								
+
 						?>
-								
+
 							<div id="site-name" class="widget" data-redirectafterdelete="<?php echo apply_filters('plchf_msb_my_sites_link', get_bloginfo('url') . '/wp-admin/admin.php?page=pluginchiefmsb'); ?>" data-widgetid="<?php echo $site->ID; ?>" data-siteid="<?php echo $site->ID; ?>">
-								
+
 								<div class="widget-top">
-								
+
 									<div class="widget-title-action">
-									
+
 										<a class="widget-action hide-if-no-js" href="#"></a>
-									
+
 									</div><!-- / Widget Title Action -->
-									
+
 									<div class="widget-title">
-									
+
 										<h4><?php echo $site->post_title; ?></h4>
-									
+
 									</div><!-- / Widget Title -->
-								
+
 								</div><!-- / Widget Top -->
-								
+
 								<div class="module-inside">
-					
+
 									<?php do_action('plchf_msb_my_sites_above_site_title'); ?>
-									
+
 									<div class="clear"></div><!-- Clear -->
-								
+
 									<h3 class="section-title floatl">
-										
+
 										<?php echo $site->post_title; ?>
-									
+
 									</h3><!-- End Title -->
-									
+
 									<a class="button-primary button btn btn-primary floatr" href="<?php echo apply_filters( 'plchf_msb_edit_sites_page', get_bloginfo('url') . '/wp-admin/admin.php' ); ?>?page=pluginchiefmsb/edit-site&mobilesite_site_id=<?php echo $site->ID; ?>">Edit Site</a>
-									
+
 									<a class="button-primary button btn btn-primary floatr deletesite" data-siteid="<?php echo $site->ID; ?>" href="#">Delete Site</a>
-									
+
 									<div class="clear"></div>
-									
+
 									<?php do_action('plchf_msb_my_sites_below_site_title'); ?>
-									
+
 									<div class="clear"></div><!-- Clear -->
-									
+
 									<hr>
-									
+
 									<div class="clear"></div>
-									
+
 									<?php do_action('plchf_msb_sites_page_above_site_info'); ?>
-									
+
 									<div class="clear"></div>
-									
+
 									<div class="one_fourth">
-									
+
 										<h4>Site Preview</h4>
-										
+
 										<hr>
-										
+
 										<?php plchf_msb_qrcode_preview($home_id); ?>
-						
+
 										<?php plchf_msb_site_shortlink($home_id); ?>
-										
+
 									</div>
-									
+
 									<div class="three_fourth column-last">
-									
+
 										<div class="one_half">
-										
+
 											<h4>Pages</h4>
-											
+
 											<hr>
-											
+
 											<?php do_action('plchf_msb_sites_center_column', $siteid); ?>
-										
+
 										</div>
-										
+
 										<div class="one_half column-last">
-										
+
 											<h4>Site</h4>
-											
+
 											<hr>
-											
+
 											<?php do_action('plchf_msb_sites_right_column', $siteid); ?>
-											
-										
+
+
 										</div>
-									
+
 									</div>
-									
+
 									<div class="clear"></div>
-									
+
 									<hr>
-									
+
 									<?php do_action('plchf_msb_sites_page_below_site_info', $siteid); ?>
-								
+
 								</div><!-- / Module Inside -->
-							
+
 							</div><!-- /Widget -->
-						
+
 						<?php
-						
+
 						}
-						
+
 					} else {
-					
+
 					?>
-					
+
 					<!-- If No Sites Exist -->
-						
+
 						<div class="no-sites">
-						
-							<?php 
-								
-								$output .= '<h1>Oh No! You Don\'t Have any Mobile Sites!';
+
+							<?php
+
+								$output  = '<h1>Oh No! You Don\'t Have any Mobile Sites!';
 								$output .= '<h3>Go Ahead and Create One. It\'s Easy!</h3>';
-								
+
 								$output .= '<a class="button-primary button btn btn-primary floatc" href="'. apply_filters('plchf_msb_new_sites_page', 'admin.php?page=pluginchiefmsb/new-site' ).'">'. apply_filters('plchf_msb_create_new_site_link_text', 'Create New Site').'</a>';
-								
+
 								echo apply_filters('plchf_msb_no_sites_message', $output);
-							
+
 							?>
-						
+
 						</div>
-					
+
 					<?php
-						
+
 					}
-					
+
 					?>
-					
+
 					<div class="clear"></div><!-- Clear -->
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
-		
+
 		<?php get_pluginchiefmsb_footer(); ?>
 
-	<?php 
-		
+	<?php
+
 	}
