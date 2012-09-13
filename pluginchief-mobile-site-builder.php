55d90bd7f848e743ec3f6c5ff1f5005d7eb21b24
bc <mountaininja@gmail.com>
2012-09-12 23:44:38 -0600

    Plugin updater

--- pluginchief-mobile-site-builder.php
+++ pluginchief-mobile-site-builder.php
@@ -8,10 +8,10 @@ Author: Visioniz, Jason Bahl, Brandon Camenisch
 Author URI: http://visioniz.com/
 License: GPLv2 or later
 
-____   ____.__       .__              .__        
+____   ____.__       .__              .__
 \   \ /   /|__| _____|__| ____   ____ |__|_______
  \   Y   / |  |/  ___/  |/  _ \ /    \|  \___   /
-  \     /  |  |\___ \|  (  <_> )   |  \  |/    / 
+  \     /  |  |\___ \|  (  <_> )   |  \  |/    /
    \___/   |__/____  >__|\____/|___|  /__/_____ \
                    \/               \/         \/
 
@@ -22,48 +22,62 @@ ____   ____.__       .__              .__
 /* ---------------------------------------------------------------------------- */
 
 	// Plugin Structure:
-	
+
 	// functions/ - directory to help abstract common functions, for organization
 	// themes/ - directory to house built-in themes
 	// settings/ - directory to house built-in settings pages
 	// page-elements/ - directory to house built-in page elements
-	
+
 /* ---------------------------------------------------------------------------- */
 /* Set Up Plugin Constants
 /* ---------------------------------------------------------------------------- */
 
 	// NOTE: PLUGINCHIEFMSB = Plugin Chief Mobile Site Builder
-	
+
 	// Url to plugin Folder
 	define('PLUGINCHIEFMSB', plugin_dir_url(__FILE__));
-	
+
 	// Path to Plugin Folder
 	define('PLUGINCHIEFMSB_PATH', plugin_dir_path(__FILE__));
-	
+
 	// URL to Plugin Folder
 	$pluginchiefmsbdir = plugin_dir_url(__FILE__);
 	global $pluginchiefmsbdir;
-	
+
 	// Gets options from the plugin options table
 	$plchf_msb_options = get_option('plugin_chief_msb');
-	global $plchf_msb_options;	
+	global $plchf_msb_options;
 
 /* ---------------------------------------------------------------------------- */
 /* Inlcude Plugin Files
 /* ---------------------------------------------------------------------------- */
-	
-	// This includes all top-level files within the plugin sub-directories. 
+
+	// This includes all top-level files within the plugin sub-directories.
 	// Any deeper files that need to be included, need to be included from those files.
 	foreach (glob(PLUGINCHIEFMSB_PATH . "/*/*.php") as $files){
 		require_once $files;
 	}
-	
+
 /* ---------------------------------------------------------------------------- */
 /* Inlcude Built-In Theme Files
 /* ---------------------------------------------------------------------------- */
-	
-	// This includes all top-level files within the plugin sub-directories. 
+
+	// This includes all top-level files within the plugin sub-directories.
 	// Any deeper files that need to be included, need to be included from those files.
 	foreach (glob(PLUGINCHIEFMSB_PATH . "/mobile-themes/*/*.php") as $files){
 		require_once $files;
 	}
+
+/* ---------------------------------------------------------------------------- */
+/* Updater
+/* ---------------------------------------------------------------------------- */
+
+	function plchf_msb_stats_add_on_plugin_updater() {
+
+		$PluginChief_MSB_Stats_AddOn = new PluginUpdateChecker( 'http://pluginchief.com/plugin-updates/downloads/mobilechief/msb.json', __FILE__);
+
+	}
+	add_action('plugins_loaded','plchf_msb_stats_add_on_plugin_updater');
+
+
+?>
