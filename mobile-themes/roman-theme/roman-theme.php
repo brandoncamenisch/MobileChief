d79f698a14bd6372b7795f6029773fd2f1473425
bc <mountaininja@gmail.com>
2012-09-15 00:50:43 -0600

    Added bootstrap less files… Changed pirate to roman

--- mobile-themes/roman-theme/roman-theme.php
+++ mobile-themes/roman-theme/roman-theme.php
@@ -2,47 +2,47 @@
 
 /* ----------------------------------------------------------------------------
 	Register Theme
----------------------------------------------------------------------------- */	
-		
+---------------------------------------------------------------------------- */
+
 		$plchf_msb_themes[] = array(
 			'Theme'		=> array(
-				'Theme Name' 	=> 'Default Theme',
-				'Slug'			=> 'default_theme',
+				'Theme Name' 	=> 'Roman Theme',
+				'Slug'			=> 'roman_theme',
 				'Version'		=> '1.0',
 				'Author Name'	=> 'Jason Bahl',
 				'Author URL'	=> 'http://visioniz.com',
-				'Theme Path'	=> PLUGINCHIEFMSB_PATH . '/mobile-themes/default-theme/',
-				'Theme Root'	=> PLUGINCHIEFMSB . 'mobile-themes/default-theme/',
+				'Theme Path'	=> PLUGINCHIEFMSB_PATH . '/mobile-themes/roman-theme/',
+				'Theme Root'	=> PLUGINCHIEFMSB . 'mobile-themes/roman-theme/',
 				'Multiple Pages'=> 'Yes',
-				'Screenshot'	=> 'default_screenshot.png',
+				'Screenshot'	=> 'roman_screenshot.png',
 				'Page Elements'	=> 'Yes',
 				'Settings Panel'=> 'Yes',
 				'Description'	=> 'Jarred smells'
 			),
 		);
-		
+
 /* ----------------------------------------------------------------------------
 	Set the Theme Root
----------------------------------------------------------------------------- */	
-	
-	function plchf_msb_default_theme_redirect_theme(){
-		
+---------------------------------------------------------------------------- */
+
+	function plchf_msb_roman_theme_redirect_theme(){
+
 		// Set the Theme Root
 		return dirname( __FILE__ ) . '/theme/';
-		
+
 	}
-	
-	add_filter('plchf_msb_theme_root_default_theme','plchf_msb_default_theme_redirect_theme');
+
+	add_filter('plchf_msb_theme_root_roman_theme','plchf_msb_roman_theme_redirect_theme');
 
 /* ----------------------------------------------------------------------------
 	Set the Current Page
 ---------------------------------------------------------------------------- */
-	
-	function plchf_msb_default_theme_redirect_page(){
-		
+
+	function plchf_msb_roman_theme_redirect_page(){
+
 		// Set the Page Template
 		return 'standard.php';
-		
+
 	}
-	
-	add_filter('plchf_msb_theme_page_default_theme','plchf_msb_default_theme_redirect_page');
+
+	add_filter('plchf_msb_theme_page_roman_theme','plchf_msb_roman_theme_redirect_page');
--- mobile-themes/roman-theme/theme/header.php
+++ mobile-themes/roman-theme/theme/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  
-  	<?php 
+
+  	<?php
   	global $pluginchiefmsbdir, $post, $wp_query;
   	$post_id 	= $wp_query->post->ID;
   	$post_id 	= get_post($post_id)->post_parent;
   	$meta 		= get_post_custom( $post_id );
   	$sitetheme 	= $meta['_plchf_msb_site_theme'][0];
   	?>
-  	
+
     <meta charset="utf-8">
     <title>Clean Theme</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,10 +17,8 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="<?php echo $pluginchiefmsbdir; ?>theme-assets/css/bootstrap.css" rel="stylesheet">
-    <link href="<?php echo $pluginchiefmsbdir; ?>theme-assets/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="<?php echo $pluginchiefmsbdir; ?>css/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="<?php echo $pluginchiefmsbdir; ?>mobile-themes/default-theme/theme/css/style.css" rel="stylesheet">
+    <link href="<?php echo $pluginchiefmsbdir; ?>mobile-themes/roman-theme/theme/css/style.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -37,69 +35,69 @@
   </head>
 
   <body data-spy="scroll" data-target=".subnav" data-offset="50">
-  
+
   <!-- Navbar
     ================================================== -->
     <div class="navbar navbar-fixed-top">
-      
+
       <div class="navbar-inner">
-      
+
         <div class="container">
-      
+
           <button type="button"class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-      
+
           <a class="brand" href="<?php echo get_permalink(); ?>"><?php echo str_ireplace('_',' ', $sitetheme); ?></a>
-      
+
           <div class="nav-collapse collapse">
-      
+
             <ul class="nav">
-      
+
               <li class="active">
                 <a href="<?php echo get_permalink(); ?>">Overview</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Scaffolding</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Base CSS</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Components</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Javascript plugins</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Using LESS</a>
               </li>
-      
+
               <li class="<?php echo get_permalink(); ?>"></li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Customize</a>
               </li>
-      
+
               <li class="">
                 <a href="<?php echo get_permalink(); ?>">Examples</a>
               </li>
-      
+
             </ul>
-      
+
           </div>
-      
+
         </div>
-      
+
       </div>
-    
+
     </div>
 
     <div class="container">
