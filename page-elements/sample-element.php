ca67de0eef808f510e9f708824d001db543585e6
bc <mountaininja@gmail.com>
2012-09-14 22:25:09 -0600

    Externalized page elements

--- page-elements/sample-element.php
+++ page-elements/sample-element.php
@@ -1,29 +1,32 @@
 <?php
 
-/* ---------------------------------------------------------------------------- */
-/* Add Element to the Page Elements Menu
-/* ---------------------------------------------------------------------------- */
+/*
+// ---------------------------------------------------------------------------- //
+// Add Element to the Page Elements Menu
+// ---------------------------------------------------------------------------- //
+
 
 	function plchf_msb_add_text_element_sample_element() {
-		
+
 		// Add Page Element to the Menu
 		plchf_msb_add_page_element('Sample Element');
-		
+
 	}
-	
+
 	add_action('plchf_msb_content_elements','plchf_msb_add_text_element_sample_element');
 
-/* ---------------------------------------------------------------------------- */
-/* Create Settings for the Element
-/* ---------------------------------------------------------------------------- */
+
+// ---------------------------------------------------------------------------- //
+// Create Settings for the Element
+// ---------------------------------------------------------------------------- //
 
 function plchf_msb_page_element_settings_sample_element($count, $values){
-	
+
 	// Define Element Type
 	$element_type 	= 'Sample Element';
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'text',
 			'name'			=> 'Text',
@@ -31,11 +34,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_sample_text_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'password',
 			'name'			=> 'Password',
@@ -43,11 +46,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_password_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'slider',
 			'name'			=> 'Slider',
@@ -55,11 +58,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_slider_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'textarea',
 			'name'			=> 'Text Area',
@@ -67,11 +70,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_sample_textarea_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'colorpicker',
 			'name'			=> 'ColorPicker',
@@ -79,11 +82,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_sample_colorpicker_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'datepicker',
 			'name'			=> 'DatePicker',
@@ -91,11 +94,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_sample_datepicker_',
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'radio',
 			'name'			=> 'Radio',
@@ -107,11 +110,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 				'Option 2'	=> 'Option 2'
 			)
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'select',
 			'name'			=> 'Select',
@@ -123,11 +126,11 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 				'Option 2'	=> 'Option 2'
 			)
 		),
-	
+
 	);
-	
+
 	$fields[] = array(
-		
+
 		'field' 	=> array(
 			'type' 			=> 'wysiwyg',
 			'name'			=> 'WYSIWYG',
@@ -135,22 +138,22 @@ function plchf_msb_page_element_settings_sample_element($count, $values){
 			'placeholder'	=> 'This is some placeholder text for the text area',
 			'id' 			=> '_sample_wysiwyg_',
 		),
-	
+
 	);
-	
+
 	// Create Element Settings Panel
 	plchf_msb_page_element_settings_panel($element_type, $fields, $count, $values);
-	
+
 }
 
 add_action('plchf_msb_page_element_settings_sample_element','plchf_msb_page_element_settings_sample_element', 10, 2);
 
-/* ---------------------------------------------------------------------------- */
-/* Display Output for the Sample Element
-/* ---------------------------------------------------------------------------- */
+// ---------------------------------------------------------------------------- //
+// Display Output for the Sample Element
+// ---------------------------------------------------------------------------- //
 
 	function plchf_msb_page_element_output_sample_element($values) {
-		
+
 		echo $values['_sample_text_'].'<br/>';
 		echo $values['_password_'].'<br/>';
 		echo $values['_slider_'].'<br/>';
@@ -160,7 +163,8 @@ add_action('plchf_msb_page_element_settings_sample_element','plchf_msb_page_elem
 		echo $values['_sample_radio_'].'<br/>';
 		echo $values['_sample_select_'].'<br/>';
 		echo $values['_sample_wysiwyg_'].'<br/>';
-		
+
 	}
-	
+
 	add_action('plchf_msb_page_element_output_sample_element','plchf_msb_page_element_output_sample_element', 10, 1);
+*/
--- page-elements/section-well.php
+++ page-elements/section-well.php
@@ -5,25 +5,25 @@
 /* ---------------------------------------------------------------------------- */
 
 	function plchf_add_text_element_well_section() {
-	
+
 		plchf_msb_add_page_section('Well Section');
-		
+
 	}
-	
-	add_action('plchf_msb_social_elements','plchf_add_text_element_well_section', 1);
+
+	add_action('plchf_msb_content_elements','plchf_add_text_element_well_section', 1);
 
 /* ---------------------------------------------------------------------------- */
 /* Create Settings for the Well Section
 /* ---------------------------------------------------------------------------- */
 
 	function plchf_msb_page_element_settings_well_section($count, $values){
-		
+
 		// Define Element Type
 		$section_type 	= 'Well Section';
-		
+
 		// Define Settings Fields
 		$fields[] = array(
-			
+
 			'field' 	=> array(
 				'type' 			=> 'text',
 				'name' 			=> 'Enter some text',
@@ -31,13 +31,13 @@
 				'tooltip' 		=> 'Enter your text here. Plain Text, No HTML',
 				'placeholder' 	=> 'Enter your text to add it to the page',
 			),
-		
+
 		);
-		
+
 		// Create Element Settings Panel
 		plchf_msb_page_section_start($section_type, $fields, $count, $values);
 		plchf_msb_page_section_end($section_type, $fields, $count, $values);
-		
+
 	}
-	
+
 	add_action('plchf_msb_page_element_settings_well_section','plchf_msb_page_element_settings_well_section', 10, 2);
