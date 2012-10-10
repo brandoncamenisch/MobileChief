6e1a8bfbf21e1e7b0bd868906a8cc9a62b59f96d
Jason Bahl <jasonbahl@mac.com>
2012-10-09 20:44:55 -0600

    - plupload changes

--- page-element-settings-fields/plupload.php
+++ page-element-settings-fields/plupload.php
@@ -16,63 +16,44 @@
 		$placeholder	= $fields['field']['placeholder'];
 		$field_id		= $fields['field']['id'];
 		
-		// Get the saved Value
+		// Get Saved Values
 		$value			= $values[''.$field_id.''];
 	
-		// adjust values here
-		$id 		= $count;
-		$multiple 	= true; 	// allow multiple files upload
-		$width 		= null; 	// If you want to automatically resize all uploaded images then provide width here (in pixels)
-		$height 	= null; 	// If you want to automatically resize all uploaded images then provide height here (in pixels)
+		if ($multiple == 'true') {
+			$multiple = true;
+		} else {
+			$multiple = false;
+		}
+		
+		$id 		= $field_id; 
+		$svalue 	= $value; 	
+		$multiple 	= $multiple; 	
+		$width 		= null; 
+		$height 	= null;	
 		 
+		// Label & Tooltip
 		$output .= '
 		<label>'.$label.'
 			<a href="#" class="tipsy-se floatr" rel="tooltip" data-placement="top" data-original-title="'.$tooltip.'">
 				<img src="'.PLUGINCHIEFMSB.'images/element-icons/element-info.png" width="20px">
 			</a>
 		</label>';
-		$output .= '<input type="hidden" name="field['.$element_type.'_'.$count.']['.$field_id.']" id="'.$id.'" value="'.$value.'" />';
-		$output .= '<div class="plupload-upload-uic hide-if-no-js '; 
 			
-			if ($multiple) { 
-				$output .= 'plupload-upload-uic-multiple'; 
-			}
-		
-		$output .= '" id="'.$id.'plupload-upload-ui">';
-		
-		    $output .= '<span class="ajaxnonceplu" id="ajaxnonceplu'. wp_create_nonce($id . "pluploadan") .'"></span>';
-		    
-		    if ($width && $height) {
-		    	$output .= '<span class="plupload-resize"></span><span class="plupload-width" id="plupload-width'.$width.'"></span>';
-		        $output .= '<span class="plupload-height" id="plupload-height'.$height.'"></span>';
-		    }
-		    
-		    $output .= '<div class="filelist"></div>';
-		
-		$output .= '</div>';
-		
-		$output .= '<div class="plupload-thumbs ';
-		
-			if ($multiple) { 
-				$output .= 'plupload-thumbs-multiple';
-			} 
-		
-			$output .= '" id="'. $id .'plupload-thumbs">';
-		
-		$output .= '</div>';
-		
-			// Show Thumbs Here if there are any already
-			
-			
-		
+		$output .= '<input type="hidden" name="'.$id.'" id="'.$id.'" value="'.$svalue.'" />';
+			$output .= '<div class="plupload-upload-uic hide-if-no-js '.if ($multiple): .'plupload-upload-uic-multiple'. endif.'" id="'.$id.'plupload-upload-ui">';
+				$output .= '<input id="'.$id.'plupload-browse-button" type="button" value="'. esc_attr_e('Select Files').'" class="button" />';
+		    		$output .= '<span class="ajaxnonceplu" id="ajaxnonceplu'.wp_create_nonce($id . 'pluploadan').'">';
+		    		$output .= '</span>';
+			    	if ($width && $height):
+			    		$output .= '<span class="plupload-resize"></span>';
+			    		$output .= '<span class="plupload-width" id="plupload-width'.$width.'"></span>';
+			    		$output .= '<span class="plupload-height" id="plupload-height'.$height.'"></span>';
+			    	endif; 
+		    	$output .= '<div class="filelist"></div>';
+		    $output .= '</div>';
+		    $output .= '<div class="plupload-thumbs '.if ($multiple): .'plupload-thumbs-multiple'. endif.'" id="'.$id.'plupload-thumbs">';
+		    $output .= '</div>';
 		$output .= '<div class="clear"></div>';
-		
-		$output .= '<br/>';
-		
-		$output .= '<input id="'.$id.'plupload-browse-button" type="button" class="btn btn-primary" value="Select Files" class="button" />';
-		
-		$output .= '<div class="clear"></div>';
-		
 		$output .= '<br/>';
 		
 		echo apply_filters('plchf_msb_page_element_settings_field_plupload_filters',$output);
--- site-settings-fields/plupload.php
+++ site-settings-fields/plupload.php
@@ -13,9 +13,7 @@
 		$placeholder	= $fields['placeholder'];
 		$multiple		= $fields['multiple'];
 		$field_id		= $fields['id'];
-
-		// Get the saved Value
-		$value			= plchf_msb_get_site_option($type, $field_id);
+		$value 			= plchf_msb_get_site_option($type, $field_id);
 		
 		if ($multiple == 'true') {
 			$multiple = true;
@@ -23,94 +21,35 @@
 			$multiple = false;
 		}
 		
-		$multiple 	= $multiple; 	// allow multiple files upload
-		$width 		= null; 	// If you want to automatically resize all uploaded images then provide width here (in pixels)
-		$height 	= null; 	// If you want to automatically resize all uploaded images then provide height here (in pixels)
-	
-	
-		// JS For the PLupload Uploader
-	    $plupload_init = array(
-	        'runtimes' => 'html5, silverlight, flash, html4',
-	        'browse_button' => ''.$field_id.'plupload-browse-button', // will be adjusted per uploader
-	        'container' => ''.$field_id.'plupload-upload-ui', // will be adjusted per uploader
-	        'drop_element' => 'drag-drop-area', // will be adjusted per uploader
-	        'file_data_name' => 'async-upload', // will be adjusted per uploader
-	        'multiple_queues' => true,
-	        'url' => admin_url('admin-ajax.php'),
-	        'flash_swf_url' => includes_url('js/plupload/plupload.flash.swf'),
-	        'silverlight_xap_url' => includes_url('js/plupload/plupload.silverlight.xap'),
-	        'filters' => array(
-	        	array(
-	        		'title' => __('Allowed Files'),
-	        		'extensions' => '*')
-	        	),
-	        'multipart' => true,
-	        'urlstream_upload' => true,
-	        'multi_selection' => false, // will be added per uploader
-	        'multipart_params' => array(
-	            '_ajax_nonce' => "", // will be added per uploader
-	            'action' => 'plupload_action', // the ajax action name
-	            'imgid' => 0 // will be added per uploader
-	        )
-	    );
-
-
-	    $output = '
-	    <script type="text/javascript">
-	    var base_plupload_config='. json_encode($plupload_init) .'
-	    </script>
-	    ';
-	
-	
+		$id 		= $field_id; 
+		$svalue 	= $value; 	
+		$multiple 	= $multiple; 	
+		$width 		= null; 
+		$height 	= null;	
+		 
+		// Label & Tooltip
 		$output .= '
 		<label>'.$label.'
 			<a href="#" class="tipsy-se floatr" rel="tooltip" data-placement="top" data-original-title="'.$tooltip.'">
 				<img src="'.PLUGINCHIEFMSB.'images/element-icons/element-info.png" width="20px">
 			</a>
 		</label>';
-	
-		$output .= '<input type="hidden" name="field['.$type.$field_id.']['.$field_id.']" id="'.$field_id.'" value="'.$value.'" />';
-		$output .= '<div class="plupload-upload-uic hide-if-no-js ';
-
-			if ($multiple) {
-				$output .= 'plupload-upload-uic-multiple';
-			}
-
-		$output .= '" id="'.$field_id.'plupload-upload-ui">';
-
-		    $output .= '<span class="ajaxnonceplu" id="ajaxnonceplu'. wp_create_nonce($field_id . "pluploadan") .'"></span>';
-
-		    if ($width && $height) {
-		    	$output .= '<span class="plupload-resize"></span><span class="plupload-width" id="plupload-width'.$width.'"></span>';
-		        $output .= '<span class="plupload-height" id="plupload-height'.$height.'"></span>';
-		    }
-
-		    $output .= '<div class="filelist"></div>';
-
-		$output .= '</div>';
-
-		$output .= '<div class="plupload-thumbs ';
-
-			if ($multiple) {
-				$output .= 'plupload-thumbs-multiple';
-			}
-
-			$output .= '" id="'. $field_id .'plupload-thumbs">';
-
-		$output .= '</div>';
-
-			// Show Thumbs Here if there are any already
-
-
-
+			
+		$output .= '<input type="hidden" name="'.$id.'" id="'.$id.'" value="'.$svalue.'" />';
+			$output .= '<div class="plupload-upload-uic hide-if-no-js '.if ($multiple): .'plupload-upload-uic-multiple'. endif.'" id="'.$id.'plupload-upload-ui">';
+				$output .= '<input id="'.$id.'plupload-browse-button" type="button" value="'. esc_attr_e('Select Files').'" class="button" />';
+		    		$output .= '<span class="ajaxnonceplu" id="ajaxnonceplu'.wp_create_nonce($id . 'pluploadan').'">';
+		    		$output .= '</span>';
+			    	if ($width && $height):
+			    		$output .= '<span class="plupload-resize"></span>';
+			    		$output .= '<span class="plupload-width" id="plupload-width'.$width.'"></span>';
+			    		$output .= '<span class="plupload-height" id="plupload-height'.$height.'"></span>';
+			    	endif; 
+		    	$output .= '<div class="filelist"></div>';
+		    $output .= '</div>';
+		    $output .= '<div class="plupload-thumbs '.if ($multiple): .'plupload-thumbs-multiple'. endif.'" id="'.$id.'plupload-thumbs">';
+		    $output .= '</div>';
 		$output .= '<div class="clear"></div>';
-
-		$output .= '<br/>';
-
-		$output .= '<input id="'.$field_id.'plupload-browse-button" class="btn btn-primary" value="Select Files" type="button" />';
-
-		$output .= '<div class="clear"></div>';
-
 		$output .= '<br/>';
 
 		echo apply_filters('plchf_msb_site_settings_field_plupload_filters',$output);
