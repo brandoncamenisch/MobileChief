960b6f768f7c4e8efdbda04db5a321d4e8a1b1be
Jason Bahl <jasonbahl@mac.com>
2012-10-16 14:07:02 -0600

    - Tooltip Icons

--- site-settings-fields/icons-select.php
+++ site-settings-fields/icons-select.php
@@ -22,8 +22,8 @@ function plchf_msb_site_settings_field_icon_select($fields, $count) {
 	// Field Label
 	$output .= '
 	<label>'.$label.'
-		<a href="#" class="tipsy-se floatr info-tooltip-icon" rel="tooltip" data-placement="top" data-original-title="'.$tooltip.'">
-			<img src="'.PLUGINCHIEFMSB.'images/element-icons/element-info.png" width="20" height="20">
+		<a href="#" class="tipsy-se floatr" rel="tooltip" data-placement="top" data-original-title="'.$tooltip.'">
+			<img src="'.PLUGINCHIEFMSB.'images/element-icons/element-info.png" width="20" height="20" class="info-tooltip-icon">
 		</a>
 	</label>';
 
