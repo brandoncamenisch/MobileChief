767a47d7b6a703a318bdeb807a8eb51b6f670b2e
bc <mountaininja@gmail.com>
2012-12-21 11:34:46 -0700

    Cleaning up page elements

--- page-elements/text-editor.php
+++ page-elements/text-editor.php
@@ -1,58 +1,50 @@
 <?php
-	
-/* ---------------------------------------------------------------------------- */
-/* Add Text Editor Element to the Page Elements Menu
-/* ---------------------------------------------------------------------------- */
-
+	/**
+	 * plchf_add_text_element_text_editor function.
+	 *
+	 * @access public
+	 * @return void
+	 		Add Text Editor Element to the Page Elements Menu
+	 */
 	function plchf_add_text_element_text_editor() {
-		
-		// Add Page Element to the Menu
+		#Add Page Element to the Menu
 		plchf_msb_add_page_element('Text Editor');
-		
-	}
-	
-	add_action('plchf_msb_content_elements','plchf_add_text_element_text_editor', 2);
+	} add_action('plchf_msb_content_elements','plchf_add_text_element_text_editor', 2);
 
-/* ---------------------------------------------------------------------------- */
-/* Create Settings for the Text Editor Element
-/* ---------------------------------------------------------------------------- */
+	/**
+	 * plchf_msb_page_element_settings_text_editor function.
+	 *
+	 * @access public
+	 * @param mixed $count
+	 * @param mixed $values
+	 * @return void
+	 		Create Settings for the Text Editor Element
+	 */
+	function plchf_msb_page_element_settings_text_editor($count, $values){
+		#Define Element Type
+		$element_type 	= 'Text Editor';
+		$fields[] = array( 'field' 	=> array( 'type' 				=> 'wysiwyg',
+																					'name'				=> 'TinyMCE',
+																					'tooltip'			=> 'Enter some text here',
+																					'placeholder'	=> 'This is some placeholder text for the text area',
+																					'id' 					=> '_tinymce_'
+																					));
 
-function plchf_msb_page_element_settings_text_editor($count, $values){
-	
-	// Define Element Type
-	$element_type 	= 'Text Editor';
-	
-	$fields[] = array(
-		
-		'field' 	=> array(
-			'type' 			=> 'wysiwyg',
-			'name'			=> 'TinyMCE',
-			'tooltip'		=> 'Enter some text here',
-			'placeholder'	=> 'This is some placeholder text for the text area',
-			'id' 			=> '_tinymce_',
-		),
-	
-	);
-	
-	// Create Element Settings Panel
-	plchf_msb_page_element_settings_panel($element_type, $fields, $count, $values);
-	
-}
+		#Create Element Settings Panel
+		plchf_msb_page_element_settings_panel($element_type, $fields, $count, $values);
+	} add_action('plchf_msb_page_element_settings_text_editor','plchf_msb_page_element_settings_text_editor', 10, 4);
 
-add_action('plchf_msb_page_element_settings_text_editor','plchf_msb_page_element_settings_text_editor', 10, 4);
-
-/* ---------------------------------------------------------------------------- */
-/* Display Output for the Text Editor Element
-/* ---------------------------------------------------------------------------- */
-
+	/**
+	 * plchf_msb_page_element_output_text_editor function.
+	 *
+	 * @access public
+	 * @param mixed $values
+	 * @return void
+	 		Display Output for the Text Editor Element
+	 */
 	function plchf_msb_page_element_output_text_editor($values) {
-		
-		// Get the Values
-		$text 	= $values['_tinymce_'];
-		
-		// Output TinyMCE Content
+		#Get the Values
+		$text 	=& $values['_tinymce_'];
+		#Output TinyMCE Content
 		echo $text;
-		
-	}
-	
-	add_action('plchf_msb_page_element_output_text_editor','plchf_msb_page_element_output_text_editor', 1, 1);
+	} add_action('plchf_msb_page_element_output_text_editor','plchf_msb_page_element_output_text_editor', 1, 1);
--- page-elements/text.php
+++ page-elements/text.php
@@ -1,60 +1,50 @@
 <?php
-
-/* ---------------------------------------------------------------------------- */
-/* Add Text Element to the Page Elements Menu
-/* ---------------------------------------------------------------------------- */
-
+	/**
+	 * plchf_add_text_element_text function.
+	 *
+	 * @access public
+	 * @return void
+	 		Add Text Element to the Page Elements Menu
+	 */
 	function plchf_add_text_element_text() {
-	
 		plchf_msb_add_page_element('Text');
-		
-	}
-	
-	add_action('plchf_msb_content_elements','plchf_add_text_element_text', 1);
+	} add_action('plchf_msb_content_elements','plchf_add_text_element_text', 1);
 
-/* ---------------------------------------------------------------------------- */
-/* Create Settings for the Text Element
-/* ---------------------------------------------------------------------------- */
-
+	/**
+	 * plchf_msb_page_element_settings_text function.
+	 *
+	 * @access public
+	 * @param mixed $count
+	 * @param mixed $values
+	 * @return void
+	 		Create Settings for the Text Element
+	 */
 	function plchf_msb_page_element_settings_text($count, $values){
-		
-		// Define Element Type
+		#Define Element Type
 		$element_type 	= 'Text';
-		
-		// Define Settings Fields
-		$fields[] = array(
-			
-			'field' 	=> array(
-				'type' 			=> 'text',
-				'name' 			=> 'Enter some text',
-				'id' 			=> '_text_',
-				'tooltip' 		=> 'Enter your text here. Plain Text, No HTML',
-				'placeholder' 	=> 'Enter your text to add it to the page',
-			),
-		
-		);
-		
-		// Create Element Settings Panel
+		#Define Settings Fields
+		$fields[] = array( 'field' 	=> array( 'type' 			  => 'text',
+																					'name' 			  => 'Enter some text',
+																					'id' 			    => '_text_',
+																					'tooltip' 		=> 'Enter your text here. Plain Text, No HTML',
+																					'placeholder' => 'Enter your text to add it to the page'
+																					));
+		#Create Element Settings Panel
 		plchf_msb_page_element_settings_panel($element_type, $fields, $count, $values);
-		
-	}
-	
-	add_action('plchf_msb_page_element_settings_text','plchf_msb_page_element_settings_text', 10, 2);
+	} add_action('plchf_msb_page_element_settings_text','plchf_msb_page_element_settings_text', 10, 2);
 
-/* ---------------------------------------------------------------------------- */
-/* Display Output for the Text Element
-/* ---------------------------------------------------------------------------- */
-
+	/**
+	 * plchf_msb_page_element_output_text function.
+	 *
+	 * @access public
+	 * @param mixed $values
+	 * @return void
+	 		Display Output for the Text Element
+	 */
 	function plchf_msb_page_element_output_text($values) {
-		
-		// Get the Values
-		$text 	= $values['_text_'];
-		
-		// Output a Paragraph with the Text
+		#Get the Values
+		$text 	=& $values['_text_'];
+		#Output a Paragraph with the Text
 		$output .= '<p>'.$text.'</p>';
-		
 		echo apply_filters('plchf_msb_page_element_output_text_filter',$output);
-		
-	}
-	
-	add_action('plchf_msb_page_element_output_text','plchf_msb_page_element_output_text', 1, 1);
+	} add_action('plchf_msb_page_element_output_text','plchf_msb_page_element_output_text', 1, 1);
