66fad782918a6ddbf814b923fb24c55d324b76d2
bc <mountaininja@gmail.com>
2013-01-03 15:17:52 -0700

    Changing submenu stuff rolling back.

--- options-panel/plugin-options.php
+++ options-panel/plugin-options.php
@@ -33,25 +33,56 @@ function plchf_msb_setup_framework_options(){
 
 	$args['help_tabs'][] = array( 'id' 			=> 'plchf_msb_opts-1',
 																'title' 	=> __('Theme Information 1', 'plchf_msb_opts'),
-																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
-															);
+																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts'
+															));
+
 	$args['help_tabs'][] = array( 'id' 			=> 'plchf_msb_opts-2',
 																'title'   => __('Theme Information 2', 'plchf_msb_opts'),
-																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
-															);
+																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts'
+															));
 
 	$sections = array();
+	$id = 1;
 
 	$sections[] = array( 'icon' 	=> PLCHF_MSB__OPTIONS_URL.'img/glyphicons/glyphicons_086_display.png',
 											 'title'  => __('Front End', 'plchf_msb_opts'),
 											 'desc' 	=> __('<p class="description">This is the Description. Again HTML is allowed2</p>', 'plchf_msb_opts'),
-											 'fields' => array(
-											 #text|textarea|editor|checkbox|multi_checkbox|radio|radio_img|button_set|select|multi_select|color|date|divide|info|upload
-											 										#builtin validation includes: email|html|html_custom|no_html|js|numeric|url
-											 									)
-										 );
+											 'fields' => array(array( 'id' 			 => $id++,
+	                                              'type' 		 => 'checkbox',
+	                                              'title' 	 => __('Enable Front End Mobile Site Builder', 'plchf_msb_opts'),
+	                                              'sub_desc' => __('This will enable the front end mobile site builder allowing users to create mobile sites from the front end of your Wordpress site. You can then use the Shortcode <strong>[plchf_msb_front_end]</strong> to display on any page of your website.', 'plchf_msb_opts'),
+	                                              'desc' 		 => __('', 'plchf_msb_opts'),
+	                                              'std' 		 => '',
+	                                              'class' 	 => ''
+	                                            ),
+	                                       array( 'id' 			 => $id++,
+	                                              'type' 		 => 'checkbox',
+	                                              'title' 	 => __('Require users to register', 'plchf_msb_opts'),
+	                                              'sub_desc' => __('If checked users of your site will have to register with your site in order to create a mobile site. <strong>Note: the default user type is mobilechief-user</strong>', 'plchf_msb_opts'),
+	                                              'desc' 		 => __('', 'plchf_msb_opts'),
+	                                              'std' 		 => '',
+	                                              'class' 	 => ''
+	                                            ),
+	                                       array( 'id' 			 => $id++,
+	                                              'type' 		 => 'checkbox',
+	                                              'title' 	 => __('Enable front end download', 'plchf_msb_opts'),
+	                                              'sub_desc' => __('This will enable a shortcode <strong>[plchf_msb_front_end_download]</strong> for users to be able to download their created mobile website into a static .zip file for use anywhere other than wordpress.', 'plchf_msb_opts'),
+	                                              'desc' 		 => __('', 'plchf_msb_opts'),
+	                                              'std' 		 => '',
+	                                              'class' 	 => ''
+	                                            ),
+	                                       array( 'id' 			 => $id++,
+	                                              'type' 		 => 'checkbox',
+	                                              'title' 	 => __('Enable front end download', 'plchf_msb_opts'),
+	                                              'sub_desc' => __('If the download shortcode is enabled above then this will convert the URL\'s to relative ones and all domains matching your Wordpress URL will be removed. External URL\'s will be left alone.', 'plchf_msb_opts'),
+	                                              'desc' 		 => __('', 'plchf_msb_opts'),
+	                                              'std' 		 => '',
+	                                              'class' 	 => ''
+	                                            )));
 
-		$tabs = array();
+#text|textarea|editor|checkbox|multi_checkbox|radio|radio_img|button_set|select|multi_select|color|date|divide|info|upload
+#builtin validation includes: email|html|html_custom|no_html|js|numeric|url
+	$tabs = array();
 
 	global $PLCHF_MSB__Options;
 	$PLCHF_MSB__Options = new PLCHF_MSB__Options($sections, $args, $tabs);
