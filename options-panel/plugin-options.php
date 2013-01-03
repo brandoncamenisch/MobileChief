1a10ea988e9a4ca16a47775a74a04de168b4b453
bc <mountaininja@gmail.com>
2013-01-03 11:19:20 -0700

    Creating options for front end builder.

--- options-panel/plugin-options.php
+++ options-panel/plugin-options.php
@@ -11,48 +11,44 @@ if(!class_exists('PLCHF_MSB__Options')){
  *
  *
  */
-
+/**
+ * plchf_msb_setup_framework_options function.
+ *
+ * @access public
+ * @return void
+ */
 function plchf_msb_setup_framework_options(){
-
 	$args = array();
 
-	// MAKE SURE THIS GETS SET TO SOMETHING UNIQUE
-	$args['opt_name'] = 'plugin_chief_msb';
-	$args['dev_mode'] = false;
+	#MAKE SURE THIS GETS SET TO SOMETHING UNIQUE
+	$args['opt_name'] 					= 'plugin_chief_msb';
+	$args['dev_mode'] 					= false;
 	$args['show_import_export'] = false;
-	$args['menu_title'] = __('Settings', 'plchf_msb_opts');
-	$args['page_title'] = __('PluginChief Mobile Site Builder Options', 'plchf_msb_opts');
-	$args['page_slug'] = 'pluginchief_mobile_site_builder_options';
-	$args['page_type'] = 'submenu';
-	$args['page_parent'] = 'pluginchiefmsb';
-	$args['allow_sub_menu'] = false;
+	$args['menu_title'] 				= __('Settings', 'plchf_msb_opts');
+	$args['page_title'] 				= __('PluginChief Mobile Site Builder Options', 'plchf_msb_opts');
+	$args['page_slug'] 					= 'pluginchief_mobile_site_builder_options';
+	$args['page_type'] 					= 'submenu';
+	$args['page_parent'] 				= 'pluginchiefmsb';
+	$args['allow_sub_menu'] 		= false;
 
-	$args['help_tabs'][] = array(
-								'id' => 'plchf_msb_opts-1',
-								'title' => __('Theme Information 1', 'plchf_msb_opts'),
-								'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
-								);
-	$args['help_tabs'][] = array(
-								'id' => 'plchf_msb_opts-2',
-								'title' => __('Theme Information 2', 'plchf_msb_opts'),
-								'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
-								);
+	$args['help_tabs'][] = array( 'id' 			=> 'plchf_msb_opts-1',
+																'title' 	=> __('Theme Information 1', 'plchf_msb_opts'),
+																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
+															);
+	$args['help_tabs'][] = array( 'id' 			=> 'plchf_msb_opts-2',
+																'title'   => __('Theme Information 2', 'plchf_msb_opts'),
+																'content' => __('<p>This is the tab content, HTML is allowed.</p>', 'plchf_msb_opts')
+															);
 
 	$sections = array();
 
-	$sections[] = array(
-					'icon' 	=> PLCHF_MSB__OPTIONS_URL.'img/glyphicons/glyphicons_107_text_resize.png',
-					'title' => __('General Settings', 'plchf_msb_opts'),
-					'desc' 	=> __('<p class="description">This is the Description. Again HTML is allowed2</p>', 'plchf_msb_opts'),
-					'fields' => array(
-
-						// text|textarea|editor|checkbox|multi_checkbox|radio|radio_img|button_set|select|multi_select|color|date|divide|info|upload
-
-						//builtin validation includes: email|html|html_custom|no_html|js|numeric|url
-
-
-						)
-					);
+	$sections[] = array( 'icon' 	=> PLCHF_MSB__OPTIONS_URL.'img/glyphicons/glyphicons_086_display.png',
+											 'title'  => __('Front End', 'plchf_msb_opts'),
+											 'desc' 	=> __('<p class="description">This is the Description. Again HTML is allowed2</p>', 'plchf_msb_opts'),
+											 'fields' => array( #text|textarea|editor|checkbox|multi_checkbox|radio|radio_img|button_set|select|multi_select|color|date|divide|info|upload
+											 										#builtin validation includes: email|html|html_custom|no_html|js|numeric|url
+											 									)
+										 );
 
 		$tabs = array();
 
@@ -63,31 +59,34 @@ function plchf_msb_setup_framework_options(){
 
 add_action('init', 'plchf_msb_setup_framework_options', 0);
 
-
-/*
- *
+/**
+ * plchf_msb_my_custom_field function.
  * Custom function for the callback referenced above
- *
+ * @access public
+ * @param mixed $field
+ * @param mixed $value
+ * @return void
  */
 function plchf_msb_my_custom_field($field, $value){
 	print_r($field);
 	print_r($value);
 
-}//function
+}
 
-
-/*
- *
+/**
+ * plchf_msb_validate_callback_function function.
  * Custom function for the callback validation referenced above
- *
+ * @access public
+ * @param mixed $field
+ * @param mixed $value
+ * @param mixed $existing_value
+ * @return void
  */
 function plchf_msb_validate_callback_function($field, $value, $existing_value){
-
 	$error = false;
 	$value =  'just testing';
 	/*
 	do your validation
-
 	if(something){
 		$value = $value;
 	}elseif(somthing else){
@@ -96,11 +95,10 @@ function plchf_msb_validate_callback_function($field, $value, $existing_value){
 		$field['msg'] = 'your custom error message';
 	}
 	*/
-
 	$return['value'] = $value;
 	if($error == true){
 		$return['error'] = $field;
 	}
 	return $return;
 
-}//function
+}
